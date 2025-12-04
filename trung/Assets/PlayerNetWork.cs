59ec996ea2637f9ce33645a60f5018b5eb97e292
Trung Nguyen <trungnguyen1221999@gmail.com>
2025-12-04 15:27:04 +0200

    add sphere and spawn

--- trung/Assets/PlayerNetWork.cs
+++ trung/Assets/PlayerNetWork.cs
@@ -1,8 +1,47 @@
-using UnityEngine;
+﻿using UnityEngine;
 using Unity.Netcode;
+using Unity.Collections;
+
 public class PlayerNetWork : NetworkBehaviour
 {
+    [SerializeField] private Transform spawnObjectPrefab;
     [SerializeField] private float moveSpeed = 5f;
+    private NetworkVariable<MyCustomData> randomNumber = new NetworkVariable<MyCustomData>(
+        new MyCustomData { _int = 56, _bool = true, message = new FixedString128Bytes("Hello") },
+        NetworkVariableReadPermission.Everyone,
+        NetworkVariableWritePermission.Owner
+    );
+
+    public struct MyCustomData : INetworkSerializable
+    {
+        public int _int;
+        public bool _bool;
+        public FixedString128Bytes message;
+        public void NetworkSerialize<T>(BufferSerializer<T> serializer) where T : IReaderWriter
+        {
+            serializer.SerializeValue(ref _int);
+            serializer.SerializeValue(ref _bool);
+            serializer.SerializeValue(ref message);
+        }
+        public override string ToString() => $"Int: {_int}, Bool: {_bool}, Message: {message}";
+    }
+
+    public override void OnNetworkSpawn()
+    {
+        if (IsOwner)
+        {
+            randomNumber.Value = new MyCustomData {
+                _int = Random.Range(1, 101),
+                _bool = Random.value > 0.5f,
+                message = new FixedString128Bytes("Random msg")
+            };
+            Debug.Log($"Assigned random number: {randomNumber.Value}");
+        }
+        randomNumber.OnValueChanged += (previousValue, newValue) =>
+        {
+            Debug.Log($"{OwnerClientId}, random number: {newValue}");
+        };
+    }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -16,6 +55,17 @@ public class PlayerNetWork : NetworkBehaviour
         if (!IsOwner) return;
         float moveX = 0f;
         float moveZ = 0f;
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Vector3 spawnPos = transform.position + transform.forward * 2f;
+            // Clamp spawn position to map bounds
+            spawnPos.x = Mathf.Clamp(spawnPos.x, -10f, 10f);
+            spawnPos.y = 0.5f; // Đặt đúng mặt phẳng map
+            spawnPos.z = Mathf.Clamp(spawnPos.z, -10f, 10f);
+            Transform spawnedObject = Instantiate(spawnObjectPrefab, spawnPos, Quaternion.identity);
+            spawnedObject.GetComponent<NetworkObject>().Spawn();
+        }
 
         // WASD
         if (Input.GetKey(KeyCode.W)) moveZ += 1f;
